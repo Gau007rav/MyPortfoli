6c435ac2ed39078d25d1ba3ad4b7527428845de7
Saurabh <SaurabhSingh@Saurabhs-MacBook-Air.local>
2024-12-13 13:40:11 +0530

    2changes

--- src/Pages/Page4.js
+++ src/Pages/Page4.js
@@ -5,10 +5,10 @@ function Page4() {
     <div className="bg-gradient-to-b from-gray-900 via-black to-gray-800 p-5 flex flex-col md:flex-row">
       {/* Left Section */}
       <div className="bg-black rounded-lg w-full md:w-1/2 flex flex-col justify-center items-start p-10 mb-5 md:mb-0">
-        <h1 className="text-white text-[60px] sm:text-[120px] md:text-[150px] lg:text-[200px] uppercase font-[anzo4] leading-[150px] sm:leading-[150px] md:leading-[200px]">
+        <h1 className="text-white text-[160px] sm:text-[150px] md:text-[180px] lg:text-[200px] uppercase font-[anzo4] leading-[150px] sm:leading-[150px] md:leading-[200px]">
           What
         </h1>
-        <h1 className="text-white text-[60px] sm:text-[120px] md:text-[150px] lg:text-[200px] uppercase font-[anzo4] leading-[150px] sm:leading-[150px] md:leading-[200px] mt-5 sm:mt-10 md:mt-10">
+        <h1 className="text-white text-[160px] sm:text-[150px] md:text-[180px] lg:text-[200px] uppercase font-[anzo4] leading-[150px] sm:leading-[150px] md:leading-[200px] mt-5 sm:mt-10 md:mt-10">
           I Do
         </h1>
       </div>
@@ -19,12 +19,12 @@ function Page4() {
           Expertise
         </h2>
         <ul className="space-y-5 text-white text-lg sm:text-xl md:text-2xl font-[anzo3] uppercase list-disc list-inside">
-          <li>MERN Stack Development</li>
+          <li>MERN Stack</li>
           <li>Frontend Development</li>
           <li>Backend Development</li>
-          <li>API Integration & Development</li>
-          <li>Database:MySql , MongoDb</li>
-          <li>Responsive Web Design,Git,GitHub</li>
+          <li>API Development</li>
+          <li>MySql,MongoDb</li>
+          <li>Git,GitHub</li>
         </ul>
       </div>
     </div>
--- src/components/Tilttext.js
+++ src/components/Tilttext.js
@@ -30,17 +30,17 @@ function Tilttext() {
       ref={tiltRef}
     >
       {/* Title Text */}
-      <h1 className="text-white font-[anzo2] text-4xl md:text-6xl lg:text-5xl uppercase">
+      <h1 className="text-gray-300 font-[anzo2] text-4xl md:text-6xl lg:text-5xl uppercase">
         I am Dark Mode
       </h1>
 
       {/* Subtitle Text */}
-      <h1 className="text-gray-300 uppercase font-[anzo3] text-5xl md:text-8xl lg:text-7xl">
+      <h1 className="text-gray-400 uppercase font-[anzo3] text-5xl md:text-8xl lg:text-7xl">
         Developer
       </h1>
 
       {/* Call to Action */}
-      <h1 className="text-white uppercase font-[anzo1] text-4xl md:text-6xl lg:text-7xl">
+      <h1 className="text-gray-300 uppercase font-[anzo1] text-4xl md:text-6xl lg:text-7xl">
         To Hire
       </h1>
     </div>
